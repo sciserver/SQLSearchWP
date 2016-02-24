c6f7143210a56e5bcc2a25a7f9f7809cefe6e58a
bonbons0220 <bsouter@jhu.edu>
2016-02-24 15:39:39 -0500

    sortablz plugin uses shortcode to enqueue js file for a page. adds ajax to allow user to sort divs by values given as data nodes. see documentation for more info.

--- js/sortablz.js
+++ js/sortablz.js
@@ -24,16 +24,24 @@
 			
 			var toggles = new Array();
 			var targets = $(".sortablz-target" , context);
-			var this_data= new Array();
 			var dataz = new Array();
 			var order = new Array();
+			var this_data= new Array();
 
-			// create an array of the toggles/data keys
+			// create an array of the sorting toggles keys
 			$( "[data-toggle=sortablz]" , context ).each( function(){
 				order.push(toggles.length);
 				toggles.push( $( this ).data( "sortablz" ) );
 			});
-			
+
+			// set the function to call when a toggle is clicked
+			$( "[data-toggle=sortablz]" , context ).each( function(){
+				$(this).click( {
+						which: $(this).data("sortablz"), 
+						context : context
+					//} , Sortablz.update );
+					} , Sortablz.safeupdate );
+			});
 			
 			// create a multi dimensional array of the sortable elements' data
 			// n-1 columns hold the values to sort by, the nth column holds the html to show.
@@ -44,30 +52,44 @@
 				dataz.push( this_data );
 			});
 			
-			// set the function to call when a toggle is clicked
-			$( "[data-toggle=sortablz]" , context ).each( function(){
-				$(this).click( {
-						which: $(this).data("sortablz"), 
-						context : context
-					} , Sortablz.update );
+			//save toggles, initilized order, and dataz
+			this[context] = { toggles: toggles, order: order, dataz: dataz};
+		
+			// manually click the selected toggle
+			$( "[data-toggle=sortablz][checked]" ).click();
+			
+		},
+				
+		safeupdate: function( event ){
+			
+			var dataz = new Array();
+			var this_data= new Array();
+			
+			// create a multi dimensional array of the sortable elements' data
+			// n-1 columns hold the values to sort by, the nth column holds the html to show.
+			$(".sortablz-target>.sortablz-contents" , event.data.context).each ( function() {
+				this_data = Array();		
+				Sortablz[event.data.context].toggles.forEach( function( e ){ this_data.push( ( $( this ).data( e ) == undefined  ? "" : $( this ).data( e ).toString() ) ) }, this );
+				this_data.push( this.outerHTML );
+				dataz.push( this_data );
 			});
 			
 			//save toggles, initilized order, and dataz
-			this[context] = { toggles: toggles, order: order, dataz: dataz};
+			Sortablz[event.data.context]['dataz'] = dataz;
 			
-			// manually click the selected toggle
-			$( "[data-toggle=sortablz][checked]" ).click();
+			Sortablz.update( event ) ;
+					
 		},
-		
+				
 		update: function( event ){
 			var tndx = Sortablz[ event.data.context ].toggles.indexOf( event.data.which );
 			var i=0;
 			var n=Sortablz[ event.data.context ].order.length;
-
+			
 			//rearrange the order
 			Sortablz[ event.data.context ].order.splice( Sortablz[ event.data.context ].order.indexOf( tndx ) , 1 );			
 			Sortablz[ event.data.context ].order.unshift( tndx );
-
+			
 			Sortablz[ event.data.context ].dataz.sort( function( a , b ) {
 				var by;
 				for (var i=0; i<Sortablz[ event.data.context ].order.length ; i++) {
